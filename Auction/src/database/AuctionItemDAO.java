e364cc95985a1b6a02ecd6bd269a47d4807cd342
bebelmax <bebelmax@bszwidv.by.lo-net2.de>
2016-03-03 08:26:04 +0100

    .

--- Auction/src/database/AuctionItemDAO.java
+++ Auction/src/database/AuctionItemDAO.java
@@ -2,6 +2,7 @@ package database;
 
 import javax.persistence.EntityManager;
 
+import auction.AuctionItem;
 import auction.IAuctionItem;
 
 public class AuctionItemDAO implements GenericDao<IAuctionItem> {
@@ -13,7 +14,7 @@ public class AuctionItemDAO implements GenericDao<IAuctionItem> {
 
 	@Override
 	public IAuctionItem findById(IAuctionItem entity, EntityManager em) {
-		return null;
+		return (IAuctionItem) em.find(AuctionItem.class, entity.getAuctionItemId());
 	}
 
 	@Override
@@ -23,7 +24,7 @@ public class AuctionItemDAO implements GenericDao<IAuctionItem> {
 
 	@Override
 	public void remove(IAuctionItem entity, EntityManager em) {
-		
+		em.remove(entity);
 	}
 
 	private static AuctionItemDAO INSTANCE = null;
